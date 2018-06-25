c8bd95bc00717e9ac02de33e4dcc8405ba09a21a
Sonny Garcia <sonnygarcia@icloud.com>
2018-06-25 09:07:43 -0400

    adds structs required to perform attacks

--- structs.go
+++ structs.go
@@ -1,8 +1,6 @@
 package gremlin
 
-import (
-	"time"
-)
+import "time"
 
 // accessToken represents the object returned by the Gremlin API when making an
 // authentication request.
@@ -15,4 +13,36 @@ type accessToken struct {
 	RenewToken       string    `json:"renew_token"`
 	Role             string    `json:"role"`
 	ExpiresAt        time.Time `json:"expires_at"`
+}
+
+// Attack command details
+type Command struct {
+	// Type should be one of the following: blackhole, cpu, io, latency, memory,
+	// package_loss, shutdown, dns, time_travel, disk, process_killer
+	Type string `json:"type"`
+
+	// Args supplied to the command should be identical to those passed to the CLI
+	// and UI. Please note that some commands have no required args.
+	Args []string `json:"args,omitempty"`
+}
+
+// Attack target details
+type Target struct {
+	// Type should either be "Random" or "Exact"
+	Type string `json:"type"`
+
+	// Exact list of hosts to target
+	Exact []string `json:"exact,omitempty"`
+
+	// Tags restrict an attack only to hosts with the corresponding kv tags.
+	Tags map[string]string `json:"tags,omitempty"`
+}
+
+// Encapsulates the details required to launch an attack
+type AttackCommand struct {
+	Command `json:"command"`
+	Target  `json:"target"`
+
+	// Labels are used to target Docker containers running on target hosts
+	Labels map[string]string `json:"labels,omitempty"`
 }
